698f22e06b7d4d2b804f0abf1829bcbf7d5dcc91
Snehalchavan12 <sneh.more88@gmail.com>
2025-04-09 14:22:44 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,7 @@
+# Program to calculate the sum of 1 to 50 numbers
+n = int(input("Enter the value of n: "))
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+print("The sum of 1 to 50 numbers is:", sum)
+
